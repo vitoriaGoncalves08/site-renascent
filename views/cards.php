faf781f53a2a123cdb674ac2c6c0f11f40c983da
Vitória Gonçalves Passos <vitoriaGoncalves08@users.noreply.github.com>
2020-12-03 17:51:39 -0300

    Fotos e Cards

---
+++ views/cards.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cards</title>
+    <link rel="stylesheet" href="../css/cards.css">
+</head>
+<body>
+    <div class="cont">
+        <div class="card">
+            <div class="imgBox">
+                <img src="../img/fotosHome/img1.jpg">
+            </div>
+            <div class="contente">
+                <h2>Card 1</h2>
+                <p>AQUI FINGE QUE TEM UM TEXTO BOM, É SÓ PRA EU VER
+                    O ESPAÇO QUE TEM, É ISSO, ATÉ MAIS</p>
+        </div>
+        </div>
+        <div class="card">
+            <div class="imgBox">
+                <img src="../img/LeonardoDaVinci.jpg">
+            </div>
+            <div class="contente">
+                <h2>Card 2</h2>
+                <p>AQUI FINGE QUE TEM UM TEXTO BOM, É SÓ PRA EU VER
+                    O ESPAÇO QUE TEM, É ISSO, ATÉ MAIS</p>
+        </div>
+        </div>
+        <div class="card">
+            <div class="imgBox">
+                <img src="../img/fotosHome/img3.jpg">
+            </div>
+            <div class="contente">
+                <h2>Card 3</h2>
+                <p>AQUI FINGE QUE TEM UM TEXTO BOM, É SÓ PRA EU VER
+                    O ESPAÇO QUE TEM, É ISSO, ATÉ MAIS</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
